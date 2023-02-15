2d3f2d3ed5409edb18fe2c3e79afa568918a048d
CelinaO0o <122901822+CelinaO0o@users.noreply.github.com>
2023-02-15 11:23:28 +0100

    added ps1

---
+++ toggle_touch.ps1
@@ -0,0 +1,7 @@
+﻿$device = Get-PnpDevice | Where-Object {$_.FriendlyName -like '*touch screen*'}
+if ($device) {
+    switch ($device.Status) {
+        'OK'    { $device | Disable-PnpDevice -Confirm:$false; break }
+        'Error' { $device | Enable-PnpDevice -Confirm:$false }
+    }
+}
